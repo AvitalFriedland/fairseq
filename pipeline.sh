42d18f1e3edce121c18cab770945c9a88576208c
avitalfriedland <friedlandavital@gmail.com>
2020-07-07 09:09:35 +0300

    fix pipeline bugs

--- pipeline.sh
+++ pipeline.sh
@@ -1,19 +1,24 @@
 src=$1
 tgt=$2
+epochs=$3
+mkdir blue-$src-$tgt
 
 for num in 20 40 60 80 100; do
-  python preprocess.py --source-lang $src --target-lang $tgt \
+  echo "================================================== Preprocessing: ${num}================================================== "
+
+  python preprocess.py --source-lang $src \
+   --target-lang $tgt \
     --trainpref data/iwslt14.tokenized.$src-"$tgt"/subsample_$num/train --validpref data/iwslt14.tokenized.$src-"$tgt"/valid --testpref data/iwslt14.tokenized.$src-"$tgt"/test \
-    --destdir data-bin/iwslt14.tokenized.$src-$tgt \
+    --destdir data-bin/iwslt14.tokenized.$src-$tgt-$num \
     --workers 20 \
     --tensorboard-logdir log
-
-
+  echo "================================================== Preprocessing: ${num} Done================================================== "
+  echo "================================================== Training: ${num} ================================================== "
   python train.py \
-    data-bin/iwslt14.tokenized.de-en \
+    data-bin/iwslt14.tokenized.$src-$tgt-$num \
     --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
-    --save-dir baseline \
-    --max-epoch 1 \
+    --save-dir baseline/$src-$tgt-$num \
+    --max-epoch $epochs \
     --optimizer adam --adam-betas "(0.9, 0.98)" --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
@@ -29,5 +34,7 @@ for num in 20 40 60 80 100; do
     --encoder-embed-dim 256 --decoder-embed-dim 256 \
     --encoder-layers 4 --decoder-layers 4 \
     --tensorboard-logdir log \
-    --log-format json | tee train_$src-$tgt-$num.log | grep valid_bleu > bleu_$src-$tgt-$num.log
-    done
+    --log-format json | tee train_$src-$tgt-$num.log | grep valid_bleu | tee  blue-$src-$tgt/bleu_$src-$tgt-$num.log
+
+    echo "================================================== Training: ${num} Done================================================== "
+  done
