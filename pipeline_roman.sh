f4decc471ba0134dc7900de1913ac834c0359f33
avitalfriedland <friedlandavital@gmail.com>
2020-08-06 23:35:51 +0300

    Add generate to roman pipeline (untested)

--- pipeline_roman.sh
+++ pipeline_roman.sh
@@ -55,6 +55,7 @@ mkdir results/bleu/bleu-$src-$tgt
     --fp16 \
     -s fr \
     -t ro \
+    --log-format json | tee results/train_$src-$tgt-$num.log
     cat tee results/test_bleu_$src-$tgt-$num.log | grep BLEU4 | tee results/bleu/test_bleu_$src-$tgt-$num.log
     echo "================================================== Evaluating: ${src}-${tgt} ${num} Done================================================== "
 
